f42fc78f2765e9898f1137450a748b4771e24d38
ChillingSoundCry <30822585+ChillingSoundCry@users.noreply.github.com>
2018-07-30 23:58:52 +0800

    Update Flisp.py

--- Flisp.py
+++ Flisp.py
@@ -1,4 +1,6 @@
 #coding=UTF-8
+import random as rd
+import msvcrt
 ptr=0
 gcptr=0
 name=[1]*100
@@ -29,7 +31,6 @@ def getnum(code):
 		x=int(code[ptr:p].encode("utf-8"))
 		ptr=p
 	return x
-	print(x,'secnd')
 def gconstant(code):
 	global ptr
 	global gcptr
@@ -92,6 +93,20 @@ def interpret(code):
 		elif(code[ptr:ptr+3]=='def'):
 			ptr+=3
 			gconstant(code)
+		elif(code[ptr:ptr+6]=='random'):
+			ptr+=6
+			delblank(code)
+			if(code[ptr]==')'):
+				return rd.random()
+			elif(code[ptr]!=')'):
+				a=getnum(code)
+				delblank(code)
+				if(code[ptr]==','):
+					ptr+=1
+					b=getnum(code)
+					delblank(code)
+					if(code[ptr]==')'):
+						return rd.randint(a,b)
 		elif(code[ptr:ptr+5]=='print'):
 			ptr+=5
 			delblank(code)
@@ -100,6 +115,8 @@ def interpret(code):
 				while(code[ptr]!='"'):
 					print(code[ptr],end='')
 					ptr+=1
+				if(code[ptr+1]=='.'):
+					print(end='\n')
 			elif(code[ptr]=='.'):
 				print(end='\n')
 			else:
@@ -110,16 +127,16 @@ def interpret(code):
 						print(gc[i],end='')
 			if(code[ptr]=='.'):
 				print(end='\n')
-		elif(code[ptr:ptr+4]=='help'):
+		elif(code[ptr:ptr+4]=='help' or code[ptr]=='?'):
 			print('暂时只有加减乘除和定义常量')
-			print('方法为def 输出为print 输入为getLine')
+			print('方法为def 输出为print 输入为getLine 随机数为random')
 		else:
 			print("!!!")		
 def main():
 	global ptr
 	ptr=0
 	code=input('->')
-	print(code)
+	#print(code)
 	result=interpret(code)
 	if(result!=None):
 		print(result)
@@ -128,5 +145,5 @@ if __name__ == '__main__':
 	print('---------------Flisp------------------')
 	print('A Lisp Like Funtion Program Lungrauge!')
 	print('------This is only a Interpreter------')
-	print('--------------v 0.5-------------------')
+	print('--------------v 0.1-------------------')
 	main()
